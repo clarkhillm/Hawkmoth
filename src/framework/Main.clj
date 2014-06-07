e7b346d565ccd7deb5e57f92b70c5c76dcabb3d5
gavin <clarkhillm@gmail.com>
2014-06-08 00:02:40 +0800

    JavaFx 3D view test....

---
+++ src/framework/Main.clj
@@ -0,0 +1,19 @@
+(ns
+  ^{:author gavin}
+  framework.Main)
+
+(def manager
+  (Thread.
+    (proxy [Runnable] []
+      (run []
+        (while true
+          (do
+            (println ".")
+            (Thread/sleep 500)
+            ))
+        ))))
+
+(.start manager)
+
+
+
