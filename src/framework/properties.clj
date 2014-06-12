c03e53aa161ec6f74ae91a624c276b0b8140526c
clarkhillm <clarkhillm@gmail.com>
2014-06-12 18:33:23 +0800

    Update properties.clj

---
+++ src/framework/properties.clj
@@ -0,0 +1,11 @@
+(ns
+  ^{:author cWX205128}
+  framework.properties)
+
+(def properties
+  {
+    :worker_path "D:\\myPrj\\fxTest\\src\\framework\\worker"
+    :data_handler {
+                    :test "TestDataHandler"
+                    }
+    })
