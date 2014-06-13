cf685772ae907bff2e9fa55c29a0150f74819edb
clarkhillm <clarkhillm@gmail.com>
2014-06-13 19:22:17 +0800

    Update manager.clj

--- src/framework/worker/manager.clj
+++ src/framework/worker/manager.clj
@@ -2,10 +2,22 @@
   ^{:author cWX205128}
   framework.worker.manager
   (:use [framework.managedResoreces])
+  (:use [framework.messageBox])
   )
 
 (defn work [name]
   (while true
-    (println "#")
+    (println @box)
     (Thread/sleep 500)
-    ))
+    (let [m (getMessage name)]
+      (if-not (nil? m)
+        (do
+          (println "manager message: " m)
+          (def mm (load-string m))
+          (case (:message mm)
+            "hello" (sendMessage (:from mm) (str {:from name :message "hello I an manager thread."}))
+            ""
+            )
+          )
+        )
+      )))
