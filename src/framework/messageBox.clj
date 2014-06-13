324c8afa011fde4380d9413fb527e2898de7b615
clarkhillm <clarkhillm@gmail.com>
2014-06-13 19:21:46 +0800

    Update messageBox.clj

--- src/framework/messageBox.clj
+++ src/framework/messageBox.clj
@@ -4,10 +4,12 @@
 
 (def box (agent {}))
 (defn box_init [threadName] (send box assoc (keyword threadName) ()))
+
 (defn sendMessage [threadName message]
   (let [messageQueue ((keyword threadName) @box)]
     (if (string? message)
-      (send box assoc (keyword threadName) (concat messageQueue (seq (str message)))))))
+      (send box assoc (keyword threadName) (concat messageQueue [message])))))
+
 (defn getMessage [threadName]
   (let [messageQueue ((keyword threadName) @box)]
     (send box assoc (keyword threadName) (rest messageQueue))
