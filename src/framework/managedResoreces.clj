422429d0a5e5fb5426b2094cc7f1c6fffca53f84
clarkhillm <clarkhillm@gmail.com>
2014-06-16 18:27:58 +0800

    Update managedResoreces.clj

--- src/framework/managedResoreces.clj
+++ src/framework/managedResoreces.clj
@@ -1,6 +1,10 @@
 (ns
   ^{:author cWX205128}
-  framework.managedResoreces)
+  framework.managedResoreces
+  (:import [java.util.concurrent ArrayBlockingQueue]))
 
 (def rigistedThread (atom ()))
 (defn rigistThread [thread] (swap! rigistedThread concat [thread]))
+(def blockingQueneMap (atom {}))
+(defn initBlockQueneMap [threadName]
+  (swap! blockingQueneMap assoc (keyword threadName) (ArrayBlockingQueue. 1)))
